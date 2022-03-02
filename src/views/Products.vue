86b7a3d5a87f7efc3bd2d9a86c4005c63e9be5cc
Tursha Arendse <turshaarendse@gmail.com>
2022-03-02 16:19:39 +0200

    Font

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="productsSection">
-    <div class="container d-flex justify-content-end mb-3 mt-5 pt-4">
+    <h1><strong>TAKE YOUR PICK!</strong></h1>
+    <div class="container d-flex justify-content-start  mb-3 mt-4 pt-2">
       <div class="d-flex w-25 ms-3">
         <label for="" class="form-label">Sort by category</label>
         <select
@@ -36,90 +37,9 @@
       </div>
     </div>
 
-    <button id="submit-btn" @click="toggleModal">Add Product</button>
+    <button id="submit-btn" @click="toggleModal">Add A New Product</button>
 
-    <!-- Button trigger modal -->
-    <!-- <button
-    type="button"
-    class="btn btn-danger"
-    data-bs-toggle="modal"
-    data-bs-target="#addProductModal"
-  >
-    Add a product
-  </button> -->
-
-    <!-- Modal -->
-    <!-- <div
-    class="modal fade"
-    id="addProductModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Add product</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">
-          <div class="mb-3">
-            <label for="addTitle" class="form-label">Title</label>
-            <input
-              class="form-control"
-              type="text"
-              name="addTitle"
-              id="addTitle"
-            />
-          </div>
-          <div class="mb-3">
-            <label for="" class="form-label">Category</label>
-            <select class="form-select" name="addCategory" id="addCategory">
-              <option value="Stiletto">Stiletto</option>
-              <option value="Platform">Platform</option>
-              <option value="Block Heel">Block Heel</option>
-            </select>
-          </div>
-          <div class="mb-3">
-            <label for="addPrice" class="form-label">Price</label>
-            <input
-              class="form-control"
-              type="text"
-              name="addPrice"
-              id="addPrice"
-            />
-          </div>
-          <div class="mb-3">
-            <label for="addImg" class="form-label">Image URL</label>
-            <input class="form-control" type="text" name="addImg" id="addImg" />
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Close
-          </button>
-          <button
-            type="button"
-            class="btn btn-primary"
-            data-bs-dismiss="modal"
-            onclick="createProduct()"
-          >
-            Create Product
-          </button>
-        </div>
-      </div>
-    </div>
-  </div> -->
-
+   
     <div class="products">
       <!-- <div v-for="product of products" :key="product.id" class="card__container"> -->
       <div class="card" v-for="product in content.products" :key="product._id">
@@ -239,9 +159,12 @@ export default {
 </script>
 
 <style>
+h1{
+  color: white;
+}
 .card:hover {
   transform: scale(1.1);
-  transition: 1s;
+  transition: 0.7s;
 }
 .products {
   display: flex;
